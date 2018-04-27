44211fb09cc76b385ab54f798d1f1fad7a6e0c61
Jared Truscott <jtruscott78@gmail.com>
2018-04-27 23:14:32 +0000

    Update bling.js

--- public/javascripts/modules/bling.js
+++ public/javascripts/modules/bling.js
@@ -2,12 +2,18 @@
 As modified by Wes Bos */
 
 const $ = document.querySelector.bind(document);
-Node.prototype.on = window.on = function (name, fn) {  this.addEventListener(name, fn);};
-NodeList.prototype.__proto__ = Array.prototype; 
-// eslint-disable-line
-NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {  
-  this.forEach((elem) => {    elem.on(name, fn);  
- });
+const $$ = document.querySelectorAll.bind(document);
+
+Node.prototype.on = window.on = function (name, fn) {
+  this.addEventListener(name, fn);
 };
 
-module.exports = { $ };
+NodeList.prototype.__proto__ = Array.prototype; // eslint-disable-line
+
+NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
+  this.forEach((elem) => {
+    elem.on(name, fn);
+  });
+};
+
+export { $, $$ };
